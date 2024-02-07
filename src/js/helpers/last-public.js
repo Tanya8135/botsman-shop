a6c5793fff105b323a6be336390c8c81f1e40680
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2024-02-08 00:53:59 +0200

    add individ-furniture (all), our-history (all), last-public (all)

---
+++ src/js/helpers/last-public.js
@@ -0,0 +1,12 @@
+// import Swiper from 'swiper';
+// import 'swiper/swiper-bundle.min.css';
+// import 'swiper/css/scrollbar';
+
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper(".swiper-container", {
+        scrollbar: {
+            el: ".swiper-scrollbar",
+            hide: true,
+        },
+    });
+});
